80a4d1429b35001df738f9b6c388277e108cd333
William Vigo <will.vigo98@gmail.com>
2026-05-28 11:20:44 -0400

    Add Table to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
+"use client"
 import People from "@/components/desktop/people"
+import BillItemsTable from "@/components/desktop/data-table/table"
+import BillSetting from "@/components/desktop/additionalCharges"
+import theme from "@/components/desktop/theme"
+import { Box, CssBaseline, ThemeProvider } from "@mui/material"
+import Summary from "@/components/desktop/summary/summary"
 
 export default function Page() {
-    return <People />
+    return (
+        <ThemeProvider theme={theme}>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 2,
+                }}
+            >
+                <People />
+                <BillSetting />
+                <BillItemsTable />
+                <Summary />
+            </Box>
+        </ThemeProvider>
+    )
 }
