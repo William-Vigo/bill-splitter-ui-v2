5014ec0920ddd71a1ce2e6e1febfc128561a5188
William Vigo <will.vigo98@gmail.com>
2026-06-15 23:47:16 -0400

    Clean up code

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 "use client"
-import People from "@/components/desktop/people"
-import BillItemsTable from "@/components/desktop/data-table/table"
-import AdditionalCharges from "@/components/desktop/additionalCharges"
+import BillConfig from "@/components/desktop/billConfig"
 import theme from "@/components/desktop/theme"
-import { Box, CssBaseline, Grid, Stack, ThemeProvider } from "@mui/material"
-import Summary from "@/components/desktop/summary/summary"
-import BillConfig from "@/components/desktop/bill-config"
+import { ThemeProvider } from "@mui/material"
 
 export default function Page() {
     return (
